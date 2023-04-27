eaf58baf7f2a3c86a060dfc2cdcc29f38b447087
ships97 <shiprasingh4796@gmail.com>
2023-04-27 19:54:06 +0530

    both pages connected

---
+++ scripts/deposit.js
@@ -0,0 +1,9 @@
+document.querySelector("#head3").addEventListener("click",withdraw);
+function withdraw(){
+    window.location.replace("withdraw.html")
+}
+
+document.querySelector("#head2").addEventListener("click",deposit);
+function deposit(){
+    window.location.replace("deposit.html")
+}
